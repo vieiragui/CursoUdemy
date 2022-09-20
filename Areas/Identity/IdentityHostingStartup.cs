72614ed39f3077ae2d81ed488d1a9f3ce0cc05c5
Guilherme Vieira <vieiragui21@gmail.com>
2022-09-19 22:02:25 -0300

    [Aula 4 ] - Instalando Identity

---
+++ Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,92 @@
+﻿using Blog.Context;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+[assembly: HostingStartup(typeof(Blog.Areas.Identity.IdentityHostingStartup))]
+namespace Blog.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) =>
+            {
+                services.AddDbContext<BlogDbContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("BlogDb")));
+
+                services.AddDefaultIdentity<IdentityUser>(options =>
+                {
+                    options.SignIn.RequireConfirmedAccount = true;
+                    options.Password.RequireNonAlphanumeric = false;
+                    options.Password.RequiredLength = 6;
+                })
+                    .AddErrorDescriber<IdentityErrorDescriberPtBr>()
+                    .AddEntityFrameworkStores<BlogDbContext>();
+            });
+        }
+    }
+
+    public class IdentityErrorDescriberPtBr : IdentityErrorDescriber
+    {
+        public override IdentityError DefaultError() =>
+            new IdentityError { Code = nameof(DefaultError), Description = $"Um erro desconhecido ocorreu." };
+
+        public override IdentityError ConcurrencyFailure() =>
+            new IdentityError { Code = nameof(ConcurrencyFailure), Description = "Falha de concorrência otimista, o objeto foi modificado." };
+
+        public override IdentityError PasswordMismatch() =>
+            new IdentityError { Code = nameof(PasswordMismatch), Description = "Senha incorreta." };
+
+        public override IdentityError InvalidToken() =>
+            new IdentityError { Code = nameof(InvalidToken), Description = "Token inválido." };
+
+        public override IdentityError LoginAlreadyAssociated() =>
+            new IdentityError { Code = nameof(LoginAlreadyAssociated), Description = "Já existe um usuário com este login." };
+
+        public override IdentityError InvalidUserName(string userName) =>
+            new IdentityError { Code = nameof(InvalidUserName), Description = $"Login '{userName}' é inválido, pode conter apenas letras ou dígitos." };
+
+        public override IdentityError InvalidEmail(string email) =>
+            new IdentityError { Code = nameof(InvalidEmail), Description = $"E-mail '{email}' é inválido." };
+
+        public override IdentityError DuplicateUserName(string userName) =>
+            new IdentityError { Code = nameof(DuplicateUserName), Description = $"Usuário '{userName}' já está sendo utilizado." };
+
+        public override IdentityError DuplicateEmail(string email) =>
+            new IdentityError { Code = nameof(DuplicateEmail), Description = $"E-mail '{email}' já está sendo utilizado." };
+
+        public override IdentityError InvalidRoleName(string role) =>
+            new IdentityError { Code = nameof(InvalidRoleName), Description = $"A permissão '{role}' é inválida." };
+
+        public override IdentityError DuplicateRoleName(string role) =>
+            new IdentityError { Code = nameof(DuplicateRoleName), Description = $"A permissão '{role}' já está sendo utilizada." };
+
+        public override IdentityError UserAlreadyHasPassword() =>
+            new IdentityError { Code = nameof(UserAlreadyHasPassword), Description = "Usuário já possui uma senha definida." };
+
+        public override IdentityError UserLockoutNotEnabled() =>
+            new IdentityError { Code = nameof(UserLockoutNotEnabled), Description = "Lockout não está habilitado para este usuário." };
+
+        public override IdentityError UserAlreadyInRole(string role) =>
+            new IdentityError { Code = nameof(UserAlreadyInRole), Description = $"Usuário já possui a permissão '{role}'." };
+
+        public override IdentityError UserNotInRole(string role) =>
+            new IdentityError { Code = nameof(UserNotInRole), Description = $"Usuário não tem a permissão '{role}'." };
+
+        public override IdentityError PasswordTooShort(int length) =>
+            new IdentityError { Code = nameof(PasswordTooShort), Description = $"Senhas devem conter ao menos {length} caracteres." };
+
+        public override IdentityError PasswordRequiresNonAlphanumeric() =>
+            new IdentityError { Code = nameof(PasswordRequiresNonAlphanumeric), Description = "Senhas devem conter ao menos um caracter não alfanumérico." };
+
+        public override IdentityError PasswordRequiresDigit() =>
+            new IdentityError { Code = nameof(PasswordRequiresDigit), Description = "Senhas devem conter ao menos um digito ('0'-'9')." };
+
+        public override IdentityError PasswordRequiresLower() =>
+            new IdentityError { Code = nameof(PasswordRequiresLower), Description = "Senhas devem conter ao menos um caracter em caixa baixa ('a'-'z')." };
+
+        public override IdentityError PasswordRequiresUpper() =>
+            new IdentityError { Code = nameof(PasswordRequiresUpper), Description = "Senhas devem conter ao menos um caracter em caixa alta ('A'-'Z')." };
+    }
+}
